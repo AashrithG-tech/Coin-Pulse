5dcca27fc63cf49610071bb53e30ea25e65d2a43
AASHRITH <aashrith@Mac.lan>
2026-01-05 16:18:29 +0530

    feat: add candlestick chart and coin overview

--- components/CandleStickChart.tsx
+++ components/CandleStickChart.tsx
@@ -1,7 +1,27 @@
 'use client';
 import React from 'react';
 
-const CandlStickChart = () => {
-  return <div>CandlStickChart</div>;
+const CandleStickChart = ({
+  children,
+  data,
+  coinId,
+  height = 360,
+  initialPeriod = 'daily',
+}: CandlestickChartProps) => {
+  const [loading, setLoading] = React.useState(false);
+  return (
+    <div id="candlestick-chart">
+      <div className="chart-header">
+        <div className="flex-1">{children}</div>
+
+        <div className="button-group">
+          <span className="text-sm mx-2 font-medium text-purple-100/50">Period</span>
+          <button key="1h" className="config-button" onClick={() => {}} disabled={loading}>
+            1 hour
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
-export default CandlStickChart;
+export default CandleStickChart;
